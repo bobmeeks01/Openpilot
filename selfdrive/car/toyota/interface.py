99e0fb5c19c46a8cea60108a8aa042f70aefb3ad
briskspirit <bs@privacy.im>
2020-12-17 19:42:39 -0500

    Try briskspirit Toyota Long V2

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -308,10 +308,28 @@ class CarInterface(CarInterfaceBase):
       ret.longitudinalTuning.kpV = [1.2, 0.8, 0.5]
       ret.longitudinalTuning.kiV = [0.18, 0.12]
     else:
+<<<<<<< HEAD
       ret.gasMaxBP = [0.]
       ret.gasMaxV = [0.5]
       ret.longitudinalTuning.kpV = [3.6, 2.4, 1.5]
       ret.longitudinalTuning.kiV = [0.54, 0.36]
+=======
+      # Testing different longitudinal settings, will apply to each car to get feedback
+      #ret.gasMaxBP = [0.]
+      #ret.gasMaxV = [0.5]
+      #ret.longitudinalTuning.kpV = [3.6, 2.4, 1.5]
+      #ret.longitudinalTuning.kiV = [0.54, 0.36]
+      # ================
+      #ret.gasMaxBP = [0.] #[0., 2.2352, 20.1168, 33.528] # 0, 5, 45, 75 mph
+      #ret.gasMaxV = [1.0] #[0.66, 0.3, 0.2, 0.075]
+      ret.longitudinalTuning.kpBP = [0., 5., 15., 35.]
+      ret.longitudinalTuning.kpV = [1.4, 1.0, 0.7, 0.5] #[1.4, 1.0, 0.8]
+      ret.longitudinalTuning.kiBP = [0., 10., 35.]
+      ret.longitudinalTuning.kiV = [0.17, 0.11, 0.07] #[0.19, 0.12]
+      ret.stoppingBrakeRate = 0.1 # reach stopping point smoothly
+      ret.startingBrakeRate = 2.0 # release brakes fast
+      # ================
+>>>>>>> 2f651703... EXPERIMENTAL Long tune v2 for all Toyotas
 
     return ret
 

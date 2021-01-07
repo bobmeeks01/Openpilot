e919c8ef3709dbd0a24d739eae6674906b02d55b
bobmeeks01 <58351215+bobmeeks01@users.noreply.github.com>
2021-01-07 15:47:28 -0700

    Acceleration Tuning

--- selfdrive/controls/lib/planner.py
+++ selfdrive/controls/lib/planner.py
@@ -25,9 +25,16 @@ _A_CRUISE_MIN_BP = [  0.,  5.,  10., 20.,  40.]
 
 # need fast accel at very low speed for stop and go
 # make sure these accelerations are smaller than mpc limits
-_A_CRUISE_MAX_V = [1.2, 1.2, 0.65, .4]
-_A_CRUISE_MAX_V_FOLLOWING = [1.6, 1.6, 0.65, .4]
-_A_CRUISE_MAX_BP = [0.,  6.4, 22.5, 40.]
+
+# Bob - lower top end acceleration.  Increase low end.
+# Also make non-following the same as following per Kegman
+# _A_CRUISE_MAX_V = [1.2, 1.2, 0.65, .4]
+# _A_CRUISE_MAX_V_FOLLOWING = [1.6, 1.6, 0.65, .4]
+# _A_CRUISE_MAX_BP = [0.,  6.4, 22.5, 40.]
+
+_A_CRUISE_MAX_V = [1.7, 1.6, 0.55, .28]
+_A_CRUISE_MAX_V_FOLLOWING = [1.7, 1.6, 0.55, .28]
+_A_CRUISE_MAX_BP = [0.,  6.4, 21.5, 33.]
 
 # Lookup table for turns
 _A_TOTAL_MAX_V = [1.7, 3.2]
